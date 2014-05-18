b192cfbff9955d64a171c8114a0133d4831a8b1b
Gaston Figueroa <gastonfig@gmail.com>
2014-05-18 08:09:49 -0700

    Began implementing new direction.

--- js/main.js
+++ js/main.js
@@ -13,16 +13,16 @@ $(document).ready(function(){
 		  decimal : ':'  
 	}
 
-	var distanceAnimation = new countUp("distance-value", 0, 4.55, 2, 1.5, options),
-		speedAnimation = new countUp("speed-value", 0, 13.42, 2, 1.5, options),
-		elevationAnimation = new countUp("elevation-value", 0, 325, 0, 1.5, options),
-		timeAnimation = new countUp("time-value", 0, 20.17, 2, 1.5, timeOptions);
+	// var distanceAnimation = new countUp("distance-value", 0, 4.52, 2, 1.5, options),
+	// 	speedAnimation = new countUp("speed-value", 0, 17.52, 2, 1.5, options),
+	// 	elevationAnimation = new countUp("elevation-value", 0, 325, 0, 1.5, options),
+	// 	timeAnimation = new countUp("time-value", 0, 15.28, 2, 1.5, timeOptions);
 
 
-	distanceAnimation.start();
-	timeAnimation.start();
-	speedAnimation.start();
-	elevationAnimation.start();
+	// distanceAnimation.start();
+	// timeAnimation.start();
+	// speedAnimation.start();
+	// elevationAnimation.start();
 
 	var elevationData = {
 	  labels : [" "," "," "," "," "],
@@ -61,8 +61,8 @@ $(document).ready(function(){
 	};
 
 	var elevationChart = $("#elevation-chart").get(0).getContext("2d");
-	new Chart(elevationChart).Line(elevationData, options);
+	// new Chart(elevationChart).Line(elevationData, options);
 
 	var speedChart = $("#speed-chart").get(0).getContext("2d");
-	new Chart(speedChart).Line(speedData, options);
+	// new Chart(speedChart).Line(speedData, options);
 });
